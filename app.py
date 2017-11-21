e8a20b469cd3c2f33006bd9fa0a3227cb78b168f
dnajafi <dbnajafi@gmail.com>
2017-11-21 16:55:36 -0700

    enable CORS

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_cors import CORS
 
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.stock import Stock, StockList 
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
